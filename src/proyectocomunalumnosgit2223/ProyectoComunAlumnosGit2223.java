5b52b5c49da7d75b64752ff4fc00de25776166b8
jeff <jeffstylees@gmail.com>
2023-04-20 11:51:46 +0200

    ACTIVIDAD 4.3.4

--- src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
+++ src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
@@ -17,8 +17,8 @@ public class ProyectoComunAlumnosGit2223 {
      }
     public static void Jeff()
     {
-        System..out.println("El Método de jeff");
+        System.out.println("El Método de jeff");
 }
-        // TODO code application logic here
+    
     }
     
