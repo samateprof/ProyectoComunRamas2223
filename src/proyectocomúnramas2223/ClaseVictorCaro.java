2b78964e3c224c418ed6fab590b36a95c0bf8cfa
Mañana <Maana@DAMDAW1-08>
2023-04-26 10:34:39 +0200

    VictorCaro

---
+++ src/proyectocomúnramas2223/ClaseVictorCaro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author Mañana
+ */
+public class ClaseVictorCaro {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Victor Caro Martinez");
+    }
+}
