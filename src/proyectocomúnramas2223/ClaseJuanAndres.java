1a32a535f06a75833626638a61a5ed080d5c493b
nanemoreno19 <juanandresmoreno508@gmail.com>
2023-04-26 10:20:22 +0200

    Actividad 4.3.6

---
+++ src/proyectocomúnramas2223/ClaseJuanAndres.java
@@ -0,0 +1,16 @@
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author Nane Moreno
+ */
+public class ClaseJuanAndres {
+    public static void main(String[] args) {
+        metodoJuanAndres();
+        
+    }
+
+    private static void metodoJuanAndres() {
+        System.out.println("Método de Juan Andrés");
+    }
+}
