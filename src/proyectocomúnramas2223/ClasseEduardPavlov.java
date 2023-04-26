c9ab2969bb9aa50452f3fce8ffe16ebbe49a1e71
Eduard <ofbeltom@gmail.com>
2023-04-26 10:28:52 +0200

    Actividad 4.3.6 Eduard Pavlov

---
+++ src/proyectocomúnramas2223/ClasseEduardPavlov.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author beltom
+ */
+public class ClasseEduardPavlov {
+        public class ClaseEduardPavlov {
+
+        public void eduardPavlov() {
+            System.out.println("Método de Eduard Pavlov");
+        }
+    }
+}
