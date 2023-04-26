f8bf244d5c838da5ef3be48a3e35af676495d633
Samueleitorme <samuelereche.smr@gmail.com>
2023-04-26 10:20:24 +0200

    Mi nombre

---
+++ src/proyectocomúnramas2223/ClaseSamuelReche.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author Mañana
+ */
+public class ClaseSamuelReche {
+ 
+    public void miNombre(){
+        System.out.println("Método de Samuel Reche");
+    }
+}
