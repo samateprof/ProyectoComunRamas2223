80f26d0568e9f500c56f8e5134f219d853a80763
Jota <Jota@DESKTOP-55K1871>
2023-04-26 10:26:46 +0200

    Creacion método Joaquín

---
+++ src/proyectocomúnramas2223/claseJoaquin.java
@@ -0,0 +1,19 @@
+package proyectocomúnramas2223;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jota
+ */
+public class claseJoaquin {
+    
+     public void mostrarNombre() {
+        String nombre = "Joaquín Hernández";
+        System.out.println("Método de: "+ nombre);
+    }
+    
+}
