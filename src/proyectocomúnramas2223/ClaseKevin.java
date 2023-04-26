15532010d0ffab797c33dbf3eb1ecfe49cf6f475
Kevin <kevnmm96@gmail.com>
2023-04-26 10:21:36 +0200

    Mi método

---
+++ src/proyectocomúnramas2223/ClaseKevin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/ClaseJava.java to edit this template
+ */
+
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ClaseKevin {
+    private String nombre;
+
+   public ClaseKevin (String nombre) {
+       this.nombre = nombre;
+       
+   }
+   public String nombre(){
+       return "Método de Kevin";
+   }
+    
+}
