86d4db83ce31ae2de0d65de74bd219a2c5964fc1
Antonio <toniperez97@gmail.com>
2023-04-26 10:26:43 +0200

    ACtividad 4.3.6

---
+++ src/proyectocomúnramas2223/ClaseAntonio.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author Mañana
+ */
+public class ClaseAntonio {
+    
+    
+    public void antonio(){
+        System.out.println("Metodo de <Antonio>");
+        
+     }
+    
+}
