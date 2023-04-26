02014e2dd1b4c7f1c2c897cc6d7d1c65ee28d3b8
kanko <juanjomoni@outlook.com>
2023-04-26 10:29:37 +0200

    Fin de la actividad

---
+++ src/proyectocomúnramas2223/ClaseJuanJoseGimenezMarin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomúnramas2223;
+
+/**
+ *
+ * @author juanjogimenezmarin
+ */
+public class ClaseJuanJoseGimenezMarin {
+    public void tuNombre() {
+        System.out.println("Método de Juan José Giménez Marín");
+    }
+}
